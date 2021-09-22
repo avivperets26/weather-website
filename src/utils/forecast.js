c4b523abb2b7ebe9e4e4224c991c3f6938af3aba
Aviv <Avivperets26@gmail.com>
2021-09-22 22:55:46 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=12dcec55dad68100435e79816f57f7d7&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=m";
+  console.log(url);
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " dagrees out, it feels like " +
+          body.current.feelslike
+      );
+    }
+  });
+};
+module.exports = forecast;
